5fadc949647b285f6409a45b9ec0ae35cc1f551f
florapetersen <floramarina@comcast.net>
2020-11-14 17:22:28 -0800

    added dance_class time method

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -7,4 +7,8 @@ class DanceClass < ApplicationRecord
   belongs_to :teacher, class_name: "User", foreign_key: :user_id
 
   validates :name, :day, :start_time, :duration, presence: true 
+
+  def time 
+    self.start_time.strftime('%b %e at %l:%M %p')
+  end
 end 
