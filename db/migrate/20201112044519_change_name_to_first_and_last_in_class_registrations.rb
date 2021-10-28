fb40e53a1059c0f68f08d5f0e15e6b6cb0447102
florapetersen <floramarina@comcast.net>
2021-10-28 05:44:50 -0700

    edit typo in migration

--- db/migrate/20201112044519_change_name_to_first_and_last_in_class_registrations.rb
+++ db/migrate/20201112044519_change_name_to_first_and_last_in_class_registrations.rb
@@ -1,6 +1,6 @@
 class ChangeNameToFirstAndLastInClassRegistrations < ActiveRecord::Migration[5.0]
   def change
-    change_column :class_registrations, :name, first_name
+    rename_column :class_registrations, :name, :first_name
     add_column :class_registrations, :last_name, :string
   end
 end
