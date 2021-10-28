45fb300f2563e8ad55c71e660f7c13ab6a40c306
florapetersen <floramarina@comcast.net>
2021-10-28 05:49:20 -0700

    remove migrations

--- db/migrate/20201112044800_remove_name_from_class_registrations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNameFromClassRegistrations < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :class_registrations, :name
-  end
-end
--- db/migrate/20201112044858_add_first_name_to_class_registrations.rb
+++
@@ -1,5 +0,0 @@
-class AddFirstNameToClassRegistrations < ActiveRecord::Migration[5.0]
-  def change
-    add_column :class_registrations, :first_name, :string
-  end
-end
